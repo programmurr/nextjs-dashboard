27714137a49cb769d4da057f5c3aa783999ae822
Conor Murray <64764695+programmurr@users.noreply.github.com>
2023-10-28 22:16:41 +0100

    Fix failing build

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -21,7 +21,7 @@ const links = [
   { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
 ];
 
-export default function NavLinks(props) {
+export default function NavLinks() {
   const pathname = usePathname();
   return (
     <>
